a8ab6fb40fbe67b7b9a5d3ec0191c33981d651df
Luchy-B <okparabon@gmail.com>
2023-04-12 10:25:08 +0100

    Add reducers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import TodoApp from './App';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <TodoApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <TodoApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
 
---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  categories: [],
+};
+
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {
+    checkStatus: (state) => {
+      if (state.categories.length === 0) {
+        return 'Under construction';
+      }
+      return 'Under construction';
+    },
+  },
+});
+
+export const { checkStatus } = categoriesSlice.actions;
+export default categoriesSlice.reducer;
