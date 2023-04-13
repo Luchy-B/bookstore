252e62fe94375f30d92f914cc6de7802ca6e3da9
Luchy-B <okparabon@gmail.com>
2023-04-13 10:35:31 +0100

    fix linters error

--- src/Components/Boolist/NewBookForm.js
+++ src/Components/Boolist/NewBookForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../../redux/Books/booksSlice';
 
 const NewBookForm = () => {
   const initialInputs = {
